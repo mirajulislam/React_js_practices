445e82e9fddbff8c6ea900cc803ff9dc0d3e73a9
mirajul_islam57 <mirajulisam7993@gmail.com>
2021-01-21 19:15:33 +0600

    router configuration ........

--- User/user/src/App.js
+++ User/user/src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import UserListComponents from './components/UserListComponents';
-
+import {BrowserRouter as Router , Route, Switch} from 'react-router-dom'
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-         <UserListComponents />
-      </div>
-      <FooterComponent />
+      <Router>    
+           <HeaderComponent />
+              <div className="container">
+                <switch>
+                  <Route path="/" component={UserListComponents}></Route>
+                  <Route path="/users" component={UserListComponents}></Route>
+                </switch>
+              </div>
+           <FooterComponent />
+      </Router>     
     </div>
     
   );
--- User/user/src/commad/command.js
+++ User/user/src/commad/command.js
@@ -1,1 +1,3 @@
 npm install bootstrap --save
+
+npm install react-router-dom
