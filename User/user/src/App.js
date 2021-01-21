6b31b24f981fc9caefc0a5c8d77a7b403a72700c
mirajul_islam57 <mirajulisam7993@gmail.com>
2021-01-21 17:45:51 +0600

    add header and footer

--- User/user/src/App.js
+++ User/user/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import UserListComponents from './components/UserListComponents';
 
 function App() {
   return (
-    <div className="container">
-     <UserListComponents />
+    <div>
+      <HeaderComponent />
+      <div className="container">
+         <UserListComponents />
+      </div>
+      <FooterComponent />
     </div>
+    
   );
 }
 
